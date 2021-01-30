d8134140a26a0343eb560192f667517eb0784245
jawieckowski <February2021>
2021-01-30 16:09:17 +0100

    test

--- src/main/java/com/programmersonly/mentorship/mentors/template/TemplateConfiguration.java
+++ src/main/java/com/programmersonly/mentorship/mentors/template/TemplateConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class TemplateConfiguration {
 
+    
     @Bean
     TemplateRepository repository(TemplateEntityRepository repository){
         return new TemplateRepository(repository);
