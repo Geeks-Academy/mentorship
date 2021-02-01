0560d29b880b18fd650968edbe0bbdd9a4aaef65
jawieckowski <February2021>
2021-02-01 19:32:47 +0100

    test first commit

--- src/main/java/com/programmersonly/mentorship/mentors/template/TemplateConfiguration.java
+++ src/main/java/com/programmersonly/mentorship/mentors/template/TemplateConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class TemplateConfiguration {
 
+    
     @Bean
     TemplateFacade templateFacade(TemplateEntityRepository repository) {
         return new TemplateFacade(repository);
